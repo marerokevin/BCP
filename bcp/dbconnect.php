889f4287775e0920ff771442df2e7d88d2342f32
Kevin Marero <marerokevin@gmail.com>
2022-01-11 12:40:50 +0800

    database edit

--- bcp/dbconnect.php
+++ bcp/dbconnect.php
@@ -1,9 +1,9 @@
 <?php
    
-    $servername = "192.168.5.169"; 
-    $username = "marerokevin"; 
-    $password = "gpi242$$$";
-    $database = "BCP";
+   $servername = "localhost"; 
+   $username = "root"; 
+   $password = "";
+   $database = "BCP";
    
     $conn = mysqli_connect($servername, $username, $password, $database);
 ?>
